014d50e4a46f17f369d32f0554bce1ce98cba8a1
Chris Hirt <chris@hirtfamily.net>
2011-07-14 11:53:20 +0700

    added a logger

---
+++ logger.py
@@ -0,0 +1,20 @@
+class Logger(object):
+
+	def __init__(self, stream, verbose, label = ''):
+		self.stream = stream
+		self.verbose = verbose
+		self.label = label
+
+	def Debug(self, msg, newline = True):
+		if self.verbose:
+			if len(self.label) > 0 and newline and len(str(msg).strip()) > 0:
+				self.stream.write('\n' + self.label + ': ')
+			self._log(msg, newline)
+
+	def Log(self, msg, newline = True):
+		self._log(msg, newline)
+
+	def _log(self, msg, newline):
+		self.stream.write(str(msg))
+		if newline:
+			self.stream.write('\n')
